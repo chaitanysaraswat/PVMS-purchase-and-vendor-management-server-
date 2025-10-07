2a2f3754f52a40ac2624736964f0dd4da7c110ec
chaitanysaraswat <chaitanyasrswt@gmail.com>
2025-10-07 20:34:25 +0530

    delete extra files

--- PVMS(purchase and vendor management server)/backend/routes/gstRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/gstController");
-
-router.get("/", controller.getGSTSummary);
-router.get("/:vendor_id", controller.getVendorGST);
-
-module.exports = router;
